0bb94f10c72bb0b1e18b64971dd46550ba0f992e
JhuneClavio <jhune.a.s.clavio@gmail.com>
2024-04-15 02:34:27 +0800

    Move files

---
+++ src/js/03-feedback.js
@@ -0,0 +1,47 @@
+import throttle from "lodash.throttle";
+
+const form = document.querySelector('form.feedback-form');
+const emailEl = document.querySelector('label [name="email"]');
+const messageEl = document.querySelector('label [name="message"]');
+
+const STORAGE_KEY = 'feedback-form-state';
+
+function onPageReload() {
+    const savedMesssage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    if (savedMesssage) {
+        emailEl.value = savedMesssage.email;
+        messageEl.value = savedMesssage.message;
+    }
+}
+
+onPageReload();
+
+function onFormInput() {
+    const email = emailEl.value;
+    const message = messageEl.value;
+
+    const formData = {
+        email,
+        message,
+    };
+
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+form.addEventListener('input', throttle(onFormInput, 500));
+
+function onFormSubmit(e) {
+    e.preventDefault();
+    const email = emailEl.value;
+    const message = messageEl.value;
+
+    if (email == "" || message == "") {
+        alert('Both fields cannot be blank.');
+        form.reset();
+        return;
+    }
+    form.reset();
+    localStorage.removeItem(STORAGE_KEY);
+}
+
+form.addEventListener('submit', onFormSubmit);
